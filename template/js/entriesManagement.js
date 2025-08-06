1671cb8158bec97409a664af154e13b72221eae3
ZhaoZhiJia <s22790957@gmail.com>
2025-08-06 10:56:53 +0800

    會計科目管理

--- template/js/entriesManagement.js
+++ template/js/entriesManagement.js
@@ -6,4 +6,12 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+document.getElementById('clearBtn').addEventListener('click', function () {
+    // 清空 input 欄位
+    document.getElementById('dateRange').value = '';
+    document.getElementById('summonsId').value = '';
+    document.getElementById('summary').value = '';
 
+    // 重設 select 下拉選單
+    document.getElementById('suject').selectedIndex = 0;
+});
